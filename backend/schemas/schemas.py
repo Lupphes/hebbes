79bee840cca52aa2704185113e9d339c8035a9d9
Lam Nguyen <lnguye16@gustavus.edu>
2023-10-09 07:52:00 +0200

    feat(#13): Reformat required files - Update bcrypt and public/private key for decoding/encoding
    feat(#13): Update bcrypt and public/private key for decoding/encoding
    
    Feat(#13): Changed hashing library to bcrypt/register and login/logout with JWT
    
    reformat required files
    
    feat(#13) register and login/logout with JWT

---
+++ backend/schemas/schemas.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel
+
+
+class UserBase(BaseModel):
+    username: str
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class UserLogin(UserBase):
+    password: str
+
+
+class User(UserBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
