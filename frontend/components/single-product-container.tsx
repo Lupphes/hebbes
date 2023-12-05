9e42d2818501b11c2ce43ff7cdc90063a430f5a6
mitchnederlands@live.nl <mitchnederlands@live.nl>
2023-12-05 20:04:06 +0100

    responsive price listing

--- frontend/components/single-product-container.tsx
+++ frontend/components/single-product-container.tsx
@@ -1,7 +1,55 @@
 import type { NextPage } from "next";
 import Ad from "./ad";
 
-const SingleProductContainer: NextPage = () => {
+interface SingleProductProps {
+  price: string;
+  imageSrc: string;
+}
+
+interface SingleProductList{
+  singleProducts: SingleProductProps[];
+}
+
+const findIndexOfCheapestProduct = (products: SingleProductProps[]): number => {
+  if (products.length === 0) {
+    return -1; // Return -1 if the list is empty
+  }
+
+  let lowestIndex = 0;
+
+  for (let i = 1; i < products.length; i++) {
+    const lowestPrice = parseFloat(products[lowestIndex].price || "");
+    const currentPrice = parseFloat(products[i].price || "");
+
+    if (isNaN(lowestPrice) || currentPrice < lowestPrice) {
+      lowestIndex = i;
+    }
+  }
+
+  return lowestIndex;
+};
+
+const findAveragePrice = (products: SingleProductProps[]): number => {
+  if (products.length === 0) {
+    return 0; // or throw an error, depending on your use case
+  }
+
+  let sum = 0;
+
+  for(let i = 0; i < products.length; i++){
+    sum = parseFloat(products[i].price) + sum;
+  }
+
+  // Calculate the average
+  const average = sum / products.length;
+
+  return average;
+};
+
+const SingleProductContainer: NextPage<SingleProductList> = ({ singleProducts }) => {
+  const indexOfCheapest = findIndexOfCheapestProduct(singleProducts);
+  const averagePrice = findAveragePrice(singleProducts);
+
   return (
     <div className="relative w-[1440px] h-[674px] overflow-hidden shrink-0 text-left text-base text-black font-poppins">
       <div className="absolute top-[0px] left-[0px] bg-text-white-op-100 w-[1440px] h-[616px]" />
@@ -14,6 +62,7 @@ const SingleProductContainer: NextPage = () => {
             src="/asgaard-sofa-3@2x.png"
           />
         </div>
+
         <div className="relative w-[816px] h-[482px]">
           <div className="absolute top-[0px] left-[1.3px] w-[791.7px] h-[99px] overflow-hidden text-5xl">
             <div className="absolute top-[0px] left-[0px] text-23xl inline-block w-[420.5px]">
@@ -25,16 +74,16 @@ const SingleProductContainer: NextPage = () => {
             <img
               className="absolute top-[68px] left-[245.7px] w-[39px] h-[26px] object-cover"
               alt=""
-              src="/asgaard-sofa-31@2x.png"
+              src={singleProducts[indexOfCheapest].imageSrc}
             />
             <div className="absolute top-[63px] left-[0px] font-medium inline-block w-[102.1px]">
-              € 2.50
+              € {singleProducts[indexOfCheapest].price}
             </div>
             <div className="absolute top-[63px] left-[555.7px] font-medium text-darkolivegreen-100 text-right inline-block w-[236px]">
-              Avg Historical Price
+              Avg Price
             </div>
             <div className="absolute top-[63px] left-[453.7px] font-medium text-darkgray text-center inline-block w-[102.1px]">
-              € 2.52
+              € {averagePrice}
             </div>
           </div>
           <div className="absolute top-[422px] left-[9px] w-[419px] h-[60px] overflow-hidden text-darkgray">
@@ -84,211 +133,39 @@ const SingleProductContainer: NextPage = () => {
             src="/group.svg"
           />
           <div className="absolute top-[115px] left-[0px] w-[816px] h-[175px] flex flex-row items-center justify-start gap-[28px] text-center text-smi">
-            <div className="w-[57.8px] h-[175px] overflow-hidden shrink-0 flex flex-col items-center justify-start gap-[8px] text-left text-sm text-darkgray">
-              <div className="relative inline-block w-[57.8px] h-[22.3px] shrink-0">
+            <div className="relative inline-block w-[57.8px] h-[22.3px] shrink-0">
                 Shops
-              </div>
-              <div className="w-[55.7px] h-[144.2px] flex flex-col items-start justify-end pt-0 pb-[0.0000152587890625px] pr-[0.09039306640625px] pl-0 box-border gap-[1px] text-center text-2xs text-darkolivegreen-100">
-                <div className="relative w-[55.3px] h-[43.4px] overflow-hidden shrink-0">
-                  <div className="absolute top-[0px] left-[0px] rounded-8xs bg-darkolivegreen-100 w-[55.3px] h-[43.4px]" />
-                  <img
-                    className="absolute top-[7.6px] left-[7.9px] w-[40.1px] h-[28.2px] object-cover"
-                    alt=""
-                    src="/asgaard-sofa-32@2x.png"
-                  />
-                </div>
-                <div className="relative w-[55.6px] h-[99.1px] overflow-hidden shrink-0">
+            </div>
+            {singleProducts.map((product, index) => (
+              <div>
+                <div className="relative w-14 h-[116.6px]" key={index}>
+      
+                    <img
+                      className="absolute top-[0px] left-[0px] rounded-8xs w-[55.3px] h-[43.4px]"
+                      alt=""
+                      src="/rectangle-421.svg"
+                    />
+                    <img
+                      className="absolute top-[7.6px] left-[7.9px] w-[40.1px] h-[28.2px] object-cover"
+                      alt=""
+                      src={product.imageSrc}
+                    />
+                  
                   <div className="absolute top-[0px] left-[0px] text-smi text-black flex items-center justify-center w-[55.6px] h-[28.2px]">
-                    € 2.50
+                    {product.price ? `€ ${product.price}`: null}
                   </div>
-                  <div className="absolute top-[20.9px] left-[0px] flex items-center justify-center w-[55.6px] h-[28.2px]">
-                    Cheapest
+                  <div className="absolute top-[27px] left-[0px] text-2xs flex items-center justify-center w-[55.6px] h-[18.4px]">
+                    {!product.price ? ("No option") : (index == indexOfCheapest ? `Cheapest` : null)}
                   </div>
-                  <div className="absolute top-[70.9px] left-[0px] flex items-center justify-center w-[55.6px] h-[28.2px]">
-                    Deal
-                  </div>
+                  
                   <img
-                    className="absolute top-[45.2px] left-[8.3px] w-[40.1px] h-[28.2px] object-cover"
+                    className="absolute top-[88.3px] left-[7.8px] w-[40.1px] h-[28.2px] object-cover"
                     alt=""
-                    src="/asgaard-sofa-33@2x.png"
+                    src="/asgaard-sofa-313@2x.png"
                   />
                 </div>
               </div>
-            </div>
-            <div className="w-[55.6px] h-[118.5px] flex flex-col items-start justify-end pt-0 px-0 pb-[0.00005340576171875px] box-border gap-[1px] text-red">
-              <div className="w-[55.3px] h-[43.4px] overflow-hidden shrink-0 flex flex-col items-center justify-end">
-                <div className="relative rounded-8xs bg-silver w-[55.3px] h-[43.4px]" />
-                <img
-                  className="relative w-[40.1px] h-[28.2px] object-cover mt-[-35px]"
-                  alt=""
-                  src="/asgaard-sofa-34@2x.png"
-                />
-              </div>
-              <div className="relative w-[55.6px] h-[73.5px] overflow-hidden shrink-0">
-                <div className="absolute top-[0px] left-[0px] flex items-center justify-center w-[55.6px] h-[28.2px]">
-                  -
-                </div>
-                <div className="absolute top-[27px] left-[0px] text-2xs flex items-center justify-center w-[55.6px] h-[18.4px]">
-                  No option
-                </div>
-                <img
-                  className="absolute top-[45.3px] left-[8.3px] w-[40.1px] h-[28.2px] object-cover"
-                  alt=""
-                  src="/asgaard-sofa-35@2x.png"
-                />
-              </div>
-            </div>
-            <div className="relative w-[55.9px] h-[116.6px]">
-              <div className="absolute top-[45px] left-[0.3px] flex items-center justify-center w-[55.6px] h-[28.2px]">
-                € 2.55
-              </div>
-              <img
-                className="absolute top-[88.3px] left-[7.7px] w-[40.1px] h-[28.2px] object-cover"
-                alt=""
-                src="/asgaard-sofa-36@2x.png"
-              />
-              <div className="absolute top-[0px] left-[0px] w-[55.3px] h-[43.4px] overflow-hidden">
-                <div className="absolute top-[0px] left-[0px] rounded-8xs bg-silver w-[55.3px] h-[43.4px]" />
-                <img
-                  className="absolute top-[7.6px] left-[7.9px] w-[40.1px] h-[28.2px] object-cover"
-                  alt=""
-                  src="/asgaard-sofa-37@2x.png"
-                />
-              </div>
-            </div>
-            <div className="relative w-[55.6px] h-[116.6px]">
-              <img
-                className="absolute top-[0px] left-[0.2px] rounded-8xs w-[55.3px] h-[43.4px]"
-                alt=""
-                src="/rectangle-42.svg"
-              />
-              <div className="absolute top-[43.1px] left-[0px] flex items-center justify-center w-[55.6px] h-[28.2px]">
-                € 2.55
-              </div>
-              <img
-                className="absolute top-[7.6px] left-[8.1px] w-[40.1px] h-[28.2px] object-cover"
-                alt=""
-                src="/asgaard-sofa-38@2x.png"
-              />
-              <img
-                className="absolute top-[88.3px] left-[7.4px] w-[40.1px] h-[28.2px] object-cover"
-                alt=""
-                src="/asgaard-sofa-39@2x.png"
-              />
-            </div>
-            <div className="relative w-[55.7px] h-[116.6px]">
-              <div className="absolute top-[0px] left-[0px] rounded-8xs bg-silver w-[55.3px] h-[43.4px]" />
-              <div className="absolute top-[43.1px] left-[0.1px] flex items-center justify-center w-[55.6px] h-[28.2px]">
-                € 2.55
-              </div>
-              <img
-                className="absolute top-[7.6px] left-[7.9px] w-[40.1px] h-[28.2px] object-cover"
-                alt=""
-                src="/asgaard-sofa-310@2x.png"
-              />
-              <img
-                className="absolute top-[88.3px] left-[7.5px] w-[40.1px] h-[28.2px] object-cover"
-                alt=""
-                src="/asgaard-sofa-311@2x.png"
-              />
-            </div>
-            <div className="relative w-14 h-[116.6px]">
-              <img
-                className="absolute top-[0px] left-[0px] rounded-8xs w-[55.3px] h-[43.4px]"
-                alt=""
-                src="/rectangle-421.svg"
-              />
-              <div className="absolute top-[43.1px] left-[0.5px] flex items-center justify-center w-[55.6px] h-[28.2px]">
-                € 2.55
-              </div>
-              <img
-                className="absolute top-[7.6px] left-[7.9px] w-[40.1px] h-[28.2px] object-cover"
-                alt=""
-                src="/asgaard-sofa-312@2x.png"
-              />
-              <img
-                className="absolute top-[88.3px] left-[7.8px] w-[40.1px] h-[28.2px] object-cover"
-                alt=""
-                src="/asgaard-sofa-313@2x.png"
-              />
-            </div>
-            <div className="relative w-[55.6px] h-[116.6px]">
-              <div className="absolute top-[44px] left-[0px] flex items-center justify-center w-[55.6px] h-[28.2px]">
-                € 2.55
-              </div>
-              <img
-                className="absolute top-[88.3px] left-[8.3px] w-[40.1px] h-[28.2px] object-cover"
-                alt=""
-                src="/asgaard-sofa-314@2x.png"
-              />
-              <div className="absolute top-[0px] left-[0.1px] w-[55.3px] h-[43.4px] overflow-hidden">
-                <div className="absolute top-[0px] left-[0px] rounded-8xs bg-silver w-[55.3px] h-[43.4px]" />
-                <img
-                  className="absolute top-[7.6px] left-[7.9px] w-[40.1px] h-[28.2px] object-cover"
-                  alt=""
-                  src="/asgaard-sofa-315@2x.png"
-                />
-              </div>
-            </div>
-            <div className="relative w-[55.8px] h-[116.6px]">
-              <img
-                className="absolute top-[0px] left-[0px] rounded-8xs w-[55.3px] h-[43.4px]"
-                alt=""
-                src="/rectangle-421.svg"
-              />
-              <div className="absolute top-[43.1px] left-[0.3px] flex items-center justify-center w-[55.6px] h-[28.2px]">
-                € 2.55
-              </div>
-              <div className="absolute top-[7.6px] left-[7.9px] w-[40.1px] h-[28.2px]">
-                <div className="absolute top-[0px] left-[0px] w-[40.1px] h-[28.2px]" />
-                <img
-                  className="absolute top-[0px] left-[0px] w-[40.1px] h-[28.2px] object-cover"
-                  alt=""
-                  src="/asgaard-sofa-43@2x.png"
-                />
-              </div>
-              <img
-                className="absolute top-[88.3px] left-[7.6px] w-[40.1px] h-[28.2px] object-cover"
-                alt=""
-                src="/asgaard-sofa-316@2x.png"
-              />
-            </div>
-            <div className="relative w-[55.6px] h-[116.6px]">
-              <img
-                className="absolute top-[88.3px] left-[8.3px] w-[40.1px] h-[28.2px] object-cover"
-                alt=""
-                src="/asgaard-sofa-317@2x.png"
-              />
-              <div className="absolute top-[0px] left-[0px] w-[55.6px] h-[71.3px] overflow-hidden">
-                <div className="absolute top-[0px] left-[0.3px] rounded-8xs bg-silver w-[55.3px] h-[43.4px]" />
-                <div className="absolute top-[43.1px] left-[0px] flex items-center justify-center w-[55.6px] h-[28.2px]">
-                  € 2.55
-                </div>
-                <img
-                  className="absolute top-[7.6px] left-[8.2px] w-[40.1px] h-[28.2px] object-cover"
-                  alt=""
-                  src="/asgaard-sofa-318@2x.png"
-                />
-              </div>
-            </div>
-            <div className="relative w-14 h-[118.7px]">
-              <div className="absolute top-[46.4px] left-[0px] flex items-center justify-center w-[55.6px] h-[28.2px]">
-                € 2.55
-              </div>
-              <img
-                className="absolute top-[90.5px] left-[8.1px] w-[40.1px] h-[28.2px] object-cover"
-                alt=""
-                src="/asgaard-sofa-319@2x.png"
-              />
-              <div className="absolute top-[0px] left-[0.7px] w-[55.3px] h-[43.4px] overflow-hidden">
-                <div className="absolute top-[0px] left-[0px] rounded-8xs bg-silver w-[55.3px] h-[43.4px]" />
-                <img
-                  className="absolute top-[7.6px] left-[7.9px] w-[40.1px] h-[28.2px] object-cover"
-                  alt=""
-                  src="/asgaard-sofa-320@2x.png"
-                />
-              </div>
-            </div>
+            ))}
           </div>
         </div>
       </div>
