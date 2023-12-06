bd3e0a6af767c1cb1b3f2637df6eb5e34f0a8219
Ricardo Furbino <ricardofurbino@gmail.com>
2023-12-06 03:30:52 +0100

    WIP: tries to make discounts responsive

--- frontend/components/discounts.tsx
+++ frontend/components/discounts.tsx
@@ -4,15 +4,15 @@ import DiscountCard from './discount-card';
 
 const Discounts: NextPage = () => {
   return (
-    <div className="w-[1240px] overflow-hidden flex flex-col items-start justify-center text-left text-13xl text-black font-poppins md:w-auto md:[align-self:unset] md:flex-col md:gap-[0px] md:items-center md:justify-center sm:flex-col">
-      <div className="w-[1229px] h-[403px] flex flex-col items-center justify-center gap-[27px]">
+    <div className="w-[1240px] overflow-hidden flex flex-col items-start justify-center text-left text-13xl text-black font-poppins md:w-auto md:flex-col md:items-center sm:flex-col">
+      <div className="w-full h-auto flex flex-col items-center justify-center gap-[27px]">
         <div className="font-medium inline-block h-12 ml-2.5">This Week at Price Bandit!</div>
-        <div className="w-[1229px] h-[328px] overflow-hidden shrink-0 flex flex-row items-center justify-start gap-[41px] text-whitesmoke">
+        <div className="w-full h-auto overflow-hidden shrink-0 flex flex-row items-center justify-center lg:h-[1200px] md:flex-col md:gap-[0px] md:items-center md:justify-center text-whitesmoke">
           {/* First Card */}
           <DiscountCard
             title="Big Deals!"
             subtitle="Big Deals!"
-            imageUrl="/asgaard-sofa-42@2x.png"
+            imageUrl="/frame1@3x.png"
           />
           {/* Middle Card */}
           <DiscountCard
